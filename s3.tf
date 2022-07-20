e76393d45deca5f3d8f29cf261da93aa41d7e8bb
lycbrian <brian.oozou.com>
2022-07-20 13:36:41 +0700

    fix:add enable/disable for s3

--- s3.tf
+++ s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_account_public_access_block" "this" {
+  count = var.is_enabled_s3_account_public_access_block ? 1 : 0
   block_public_acls   = true
   block_public_policy = true
   restrict_public_buckets = true
